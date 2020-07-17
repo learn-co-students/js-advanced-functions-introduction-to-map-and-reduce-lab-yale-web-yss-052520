89ba1db536c036efe72ac64af9ecb49dac976228
adilgondal1 <adil.gondal@yale.edu>
2020-07-17 02:13:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+    const result = []
+    sourceArray.forEach(element => result.push(element * -1))
+    return result
+}
+function mapToNoChange(sourceArray){
+    const result = []
+    sourceArray.forEach(element => result.push(element))
+    return result
+}
+function mapToDouble(sourceArray){
+    const result = []
+    sourceArray.forEach(element => result.push(element * 2))
+    return result
+}
+function mapToSquare(sourceArray){
+    const result = []
+    sourceArray.forEach(element => result.push(element * element))
+    return result
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    sourceArray.forEach(element => startingPoint +=element)
+    return startingPoint
+}
+function reduceToAllTrue(sourceArray){
+    for (let i =0;i<sourceArray.length;i++){
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+function reduceToAnyTrue(sourceArray){
+    for (let i =0;i<sourceArray.length;i++){
+        if (sourceArray[i]) return true
+    }
+    return false
+}
