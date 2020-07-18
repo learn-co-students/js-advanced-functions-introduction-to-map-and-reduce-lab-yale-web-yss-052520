5720a3879fc95f132a1bec15facd9546e829a6b2
ZIming Mao <ziming.mao@yale.edu>
2020-07-18 14:18:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+function mapToNegativize(arr){
+    return arr.map(el => -el)
+}
+
+function mapToNoChange(arr){
+    return arr
+}
+
+function mapToDouble(arr){
+    return arr.map(el => 2*el)
+}
+
+function mapToSquare(arr){
+    return arr.map(el => el*el)
+}
+
+function reduceToTotal(arr, startingpoint = 0){
+    return arr.reduce((acc, el) => acc + el, startingpoint)
+}
+
+function reduceToAllTrue(arr){
+    let result = arr.reduce((acc, el) => acc && el, true)
+    if (result) return true
+    else return false
+}
+
+function reduceToAnyTrue(arr){
+    let result = arr.reduce((acc, el) => acc || el, false)
+    if (result) return true
+    else return false
+}
