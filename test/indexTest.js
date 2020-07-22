f48a0eeace5aaf61eb6b478bdfb9a47e08224a5b
CelestialEmperor <mapeiyao@gmail.com>
2020-07-22 21:27:40 +0100

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 describe('my own map-like methods', function() {
  describe("mapToNegativize returns an array with all values made negative", function() {
    it("transforms correctly", function(){
-      expect(mapToNegativize([1, 2, 3, -9])).to.eql([-1, -2, -3, 9])
+      expect(mapToNegativize([1, 2, 3, -9])).to.eql([-1, -2, -3, -9])
    })
 
   describe("mapToNoChange returns an array with the original values", function() {
