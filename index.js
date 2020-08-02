ef3f163db172849457b702ec51bf24a449d9577c
ev275 <enrique.valencia@yale.edu>
2020-08-02 11:44:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(num => num * -1)
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(num => num * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(num => num ** 2)
+}
+
+function reduceToTotal(array, startingPoint) {
+    if (startingPoint) {
+        return array.reduce((agg, num) => (agg + num), startingPoint )
+    } else {
+        return array.reduce((agg, num) => (agg + num))
+    }
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((agg, el) => agg = !!el)
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((agg, el) => {
+        return !!el ? true : false
+    })
+}
